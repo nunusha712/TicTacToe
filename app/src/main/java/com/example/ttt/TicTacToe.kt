3daa9e562e1e62bc3861fdd4a04b1bac271e4e7e
nunusha712 <noa.hadad712@gmail.com>
2025-12-24 19:53:09 +0200

    created game logic functions

--- app/src/main/java/com/example/ttt/TicTacToe.kt
+++ app/src/main/java/com/example/ttt/TicTacToe.kt
@@ -1,2 +1,50 @@
 package com.example.ttt
 
+class TicTacToeGame {
+    var activePlayer = "X"
+    var gameState = arrayOf("", "", "", "", "", "", "", "", "")
+    var isGameActive = true
+
+    private val winningPositions = arrayOf(
+        intArrayOf(0, 1, 2), intArrayOf(3, 4, 5), intArrayOf(6, 7, 8),
+        intArrayOf(0, 3, 6), intArrayOf(1, 4, 7), intArrayOf(2, 5, 8),
+        intArrayOf(0, 4, 8), intArrayOf(2, 4, 6)
+    )
+
+    fun makeMove(index: Int): Boolean {
+        if (gameState[index] == "" && isGameActive) {
+            gameState[index] = activePlayer
+            return true
+        }
+        return false
+    }
+
+    fun checkGameState(): String {
+        for (winPos in winningPositions) {
+            if (gameState[winPos[0]] != "" &&
+                gameState[winPos[0]] == gameState[winPos[1]] &&
+                gameState[winPos[1]] == gameState[winPos[2]]) {
+
+                isGameActive = false
+                return "WIN"
+            }
+        }
+
+        if (gameState.none { it == "" }) {
+            isGameActive = false
+            return "DRAW"
+        }
+
+        return "CONTINUE"
+    }
+
+    fun switchPlayer() {
+        activePlayer = if (activePlayer == "X") "O" else "X"
+    }
+
+    fun reset() {
+        activePlayer = "X"
+        isGameActive = true
+        gameState = arrayOf("", "", "", "", "", "", "", "", "")
+    }
+}
